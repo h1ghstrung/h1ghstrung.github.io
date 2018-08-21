da77623842565d2b0201a670e77bf4ce639ea304
h1ghstrung <pydev4hire@gmail.com>
2018-08-21 13:31:13 -0400

    Changed nav bar to be responsive on small screens, Updated color for active tab on nav bar

--- responsiveNav.js
+++ responsiveNav.js
@@ -1,3 +1,14 @@
+/* Toggle between adding and removing the "responsive" class to 
+topnav when the user clicks on the icon */
+function myRespNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
 // When the user scrolls the page, execute stickyFunction 
 window.onscroll = function() {stickyFunction()};
 
@@ -7,7 +18,8 @@ var navbar = document.getElementById("myTopnav");
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+// Add the sticky class to the navbar when you reach its scroll position.
+// Remove "sticky" when you leave the scroll position
 function stickyFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
